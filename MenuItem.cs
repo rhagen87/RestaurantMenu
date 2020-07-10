9fb60c1426269f2feabd52eb8ff09ce82fd79029
rhagen87 <reynold.hagen@gmail.com>
2020-07-10 15:59:06 -0500

    studio

---
+++ MenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private string category;
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        private bool newItem;
+        public bool NewItem
+        {
+            get { return newItem; }
+            set { newItem = value; }
+        }
+    }
+}
